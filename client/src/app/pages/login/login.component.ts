ab694ec35a09b236055d0e46d2be7a012bee02dc
SonarBoy <Joshua.L99.C@gmail.com>
2020-04-01 14:55:42 -0400

    Built For deployment

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  private user: User;
+  user: User;
 
   constructor(
     private authService:AuthService,
